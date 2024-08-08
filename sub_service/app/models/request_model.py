016a55d6d6772c6c2b1fa755f6148ee82efeded9
Vivek <vivek@teamtweaks.com>
2024-08-08 15:29:08 +0530

    delete unwanted

--- sub_service/app/models/request_model.py
+++
@@ -1,9 +0,0 @@
-
-from pydantic import BaseModel
-from typing import Optional, Dict
-
-class SupabaseRequest(BaseModel):
-    table: str
-    action: str
-    conditions: Optional[Dict] = None
-    data: Optional[Dict] = None
